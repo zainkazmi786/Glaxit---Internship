644f93d9d1829d5c9b0fc652688c9130dbc11768
zainkazmi786 <zainkazmi258@gmail.com>
2025-05-29 15:08:48 +0500

    deployment

--- week 6/Student Record System/backend/config/db.js
+++ week 6/Student Record System/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB')
+mongoose.connect('mongodb+srv://zainkazmi258:Hallian786@cluster0.eqrkuhd.mongodb.net/studentDB')
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
