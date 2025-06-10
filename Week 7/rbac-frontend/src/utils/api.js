09cfc2c46fd4cd53b4885adc036f0f6c407474cf
Syed Muhammad Zain Raza Kazmi <159058727+zainkazmi786@users.noreply.github.com>
2025-06-10 23:39:35 +0500

    Update api.js

--- Week 7/rbac-frontend/src/utils/api.js
+++ Week 7/rbac-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import.meta.env.VITE_API_URL
+const API_BASE_URL = import.meta.env.VITE_API_URL
 export const apiCall = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
