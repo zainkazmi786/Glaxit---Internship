e2bf268d68e806286a05d35352fd389802ca1cea
zainkazmi786 <zainkazmi258@gmail.com>
2025-07-08 21:21:32 +0500

    fixed a bug

--- week8/Chatterbox/chatterbox-frontend/src/components/MessageList.jsx
+++ week8/Chatterbox/chatterbox-frontend/src/components/MessageList.jsx
@@ -77,7 +77,7 @@ const MessageList = ({ room }) => {
       );
     } else if (['doc', 'docx'].includes(extension)) {
       return (
-        <svg className="w-5 h-5 text-blue-100" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <svg className="w-5 h-5 text-blue-300" fill='none' stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
         </svg>
       );
@@ -385,14 +385,14 @@ const MessageList = ({ room }) => {
 
                 {(!message.deleted && (message.type === 'file' || message.type === 'document')) && (
                   <div 
-                    className="file-attachment cursor-pointer p-3 border border-slate-200/50  rounded-lg  hover:bg-slate-50/50 dark:hover:bg-slate-600/50 transition-colors"
+                    className="file-attachment cursor-pointer p-3 border border-slate-400/50  rounded-lg  hover:bg-slate-50/50 dark:hover:bg-slate-600/50 transition-colors"
                     onClick={() => handleFileClick(message)}
                   >
                     <div className="flex items-center gap-3">
                       {getFileIcon(message.filename, message.fileType)}
                       <div className="flex-1 min-w-0">
                         <div className="font-medium text-sm truncate">{message.filename}</div>
-                        <div className="text-xs text-slate-200 ">
+                        <div className="text-xs text-slate-400 ">
                           {message.filename.split('.').pop().toUpperCase()} • Click to {message.filename.endsWith('.pdf') ? 'view' : 'download'}
                         </div>
                       </div>
